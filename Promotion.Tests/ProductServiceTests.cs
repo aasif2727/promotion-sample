95f60071755ec7362bc0ebed53fc8a229efe87a9
Asif Iqbal <33180140+aasif2727@users.noreply.github.com>
2020-06-24 19:01:54 +0530

    Service Test added
    Service Test added

---
+++ Promotion.Tests/ProductServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Promotion.Data.Entities;
+using Promotion.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Promotion.Tests
+{
+    public class ProductServiceTests
+    {
+        public ProductService productService = null;
+        public ProductServiceTests()
+        {
+            productService = new ProductService(new MemoryProvider());
+        }
+
+        [Test, Order(1)]
+        [TestCase(1,"A",50, 5)]
+        [TestCase(2, "B", 30, 5)]
+        public void Get_Discounted_Proce(int productId,string productTitle,double unitPrice, int qty)
+        {
+            Product product = new Product() { ProductId = productId, Title = productTitle, UnitPrice = unitPrice, Qty = qty };
+            List<Product> products = productService.GetProducts().ToList();
+            var results = productService.GetDiscountedPrice(product, products, null);
+            Assert.IsNotNull(results);
+            Assert.IsInstanceOf<double>(results);
+        }
+    }
+}
